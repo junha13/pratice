18c342b133b8d7ab886eec573510992855318855
junha13 <tnznf1113@gmail.com>
2025-07-19 11:42:40 +0900

    Add files via upload

---
+++ SelectInputException.java
@@ -0,0 +1,10 @@
+package project;
+
+public class SelectInputException extends Exception{
+	public SelectInputException() {
+		super("//오류가 발생하였습니다 \n오류번호 02: 문자입력 오류 \n문자를 제외하고 숫자를 입력해 주세요//");
+	}
+}
+
+
+
---
+++ SelectNumberException.java
@@ -0,0 +1,10 @@
+package project;
+
+public class SelectNumberException extends Exception{
+	public SelectNumberException() {
+		super("//오류가 발생하였습니다 \n오류번호 01: 번호입력 오류 \n범위에 맞는 숫자를 입력해 주세요//");
+	}
+}
+
+
+
